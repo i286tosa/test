900b4aa6e4095ac3d5aa6a35ee6b72612e6dea51
i286tosa <138637261+i286tosa@users.noreply.github.com>
2023-07-19 10:53:22 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-int myRoot(double x) // x の平方根を返す関数。x >= 0 ではないとき exit(1) とする。 
+double myRoot(double x) // x の平方根を返す関数。x >= 0 ではないとき exit(1) とする。 
 {
   int y;
-  if( x <= 0 ){
+  if( !(x >= 0) ){
     exit(1);
   }
   y = sqrt(x);
